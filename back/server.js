3d471aab2656763b804be7150006518e2664942d
Niranjan-23 <niranjan230525@gmail.com>
2025-03-07 18:19:47 +0530

    cors update

--- back/server.js
+++ back/server.js
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
-    credentials: true,
-  })
+  cors()
 );
 
 // Database Connection
